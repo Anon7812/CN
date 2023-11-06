836becf28aad2b96f0b227baab83a406a6da4b91
Sudeep <sudeep7217@gmail.com>
2023-11-06 22:19:03 +0530

    minor

--- tcpc.py
+++ tcpc.py
@@ -1,25 +1,12 @@
 import socket
-
-def client():
-    host = '127.0.0.1'
-    port = 8085
-
-    sock = socket.socket()
-    sock.connect((host,port))
-
-   
-    while True:
-        filename = input("Enter the filename to be shared:")
-        try:
-            fo = open(filename,"r")
-            data = fo.read()
-            if not data:
-                break
-            sock.send(data.encode())
-            fo.close()
-        except IOError:
-            print("File does not exist")
-            break
-
-if __name__ == "__main__":
-    client()
+host = '127.0.0.1'
+port = 8085
+cl = socket.socket()
+cl.connect((host,port))
+filename = input("Enter the filename to be shared: ")
+fi = open(filename, "r")
+data = fi.read()
+cl.send(data.encode())
+print("File sent successfully")
+fi.close()
+cl.close()
--- tcps.py
+++ tcps.py
@@ -1,36 +1,14 @@
 import socket
-
-def server():
-    host = '127.0.0.1'
-    port = 8085
-
-    sock = socket.socket()
-    sock.bind((host,port))
-    sock.listen(3)
-
-    connect = []
-    print("Server is running...")
-    conn = sock.accept()
-
-    connect.append(conn)
-    print("Connected with client")
-
-    fno = 0
-    idx = 0
-    for conn in connect:
-        idx += 1
-        filename = "output"+str(fno)+".txt"
-        fno += 1
-        data = conn[0].recv(1024).decode()
-        fo = open(filename,"w")
-        fo.write(data)
-        print("Receiving from client")
-        print("Received data")
-        fo.close()
-        print("Data stored in file ",filename)
-    for conn in connect:
-        conn[0].close()
-
-
-if __name__ == "__main__":
-    server()
+host = '127.0.0.1'
+port = 8085
+serv = socket.socket()
+serv.bind((host,port))
+serv.listen(3)
+print("Server is running...")
+conn, _ = serv.accept()
+data = conn.recv(1024).decode()
+filename = "output.txt"
+fo = open(filename, "w")
+fo.write(data)
+print("File received successfully")
+fo.close()
